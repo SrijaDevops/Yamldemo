1b81037aae0ab8fe23a61b1f5afdcdff388259ac
SrijaDevops <79896577+SrijaDevops@users.noreply.github.com>
2021-11-09 14:47:20 +0530

    Add files via upload

---
+++ powershelldemo.ps1
@@ -0,0 +1,7 @@
+﻿$path = "C:\Users\User\Documents\exps.xlsx"
+
+$Excel = New-Object -Com Excel.Application
+$Excel.Visible = $true
+$Workbook = $Excel.Workbooks.Open($Path)
+$page = 'Sheet2'
+$ws = $Workbook.worksheets | where-object {$_.Name -eq $page}
